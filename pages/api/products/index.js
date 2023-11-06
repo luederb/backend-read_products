b1c031eb9619835c0bedfcba4757135400825dd4
Lueder Budde <lueder.budde@gmail.com>
2023-11-06 12:32:03 +0100

    get data for productlist and product details from mongoDB

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,5 +1,15 @@
-import { products } from "../../../lib/products";
+// import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const producs = await Product.find();
+    return response.status(200).json(producs);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
+  // return response.status(200).json(products);
 }
