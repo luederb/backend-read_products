a1859676c2746b58075f1dbf73b3257870f372eb
Lueder Budde <lueder.budde@gmail.com>
2023-11-07 14:34:36 +0100

    insert edit button

--- components/Product/index.js
+++ components/Product/index.js
@@ -2,13 +2,48 @@ import useSWR from "swr";
 import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
+import { StyledButton } from "../Button/Button.styled";
+import ProductForm from "../ProductForm";
+import { useState } from "react";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
 
+  const [isEditMode, setIsEditMode] = useState(true);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    } else {
+      console.log(response.status);
+    }
+  }
+  // async function handleDeleteProduct(id) {
+  //   const response = await fetch(`api/products/${id}`, {
+  //     method: "DELETE",
+  //   });
+  //   if (response.ok) {
+  //     router.push("/");
+  //   } else {
+  //     console.log(response.status);
+  //   }
+  // }
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
@@ -17,18 +52,7 @@ export default function Product() {
     return;
   }
   console.log("data: ", data);
-  // let reviewHeadline = "No Review available";
-  // let reviewTitle = "";
-  // let reviewText = "";
-  // let reviewRating = null;
-  // if (data.reviews[0]) {
-  //   reviewHeadline = "Review:";
-  //   reviewTitle = `Title: ${data.reviews[0].title}`;
-  //   reviewText = `Text: ${data.reviews[0].text}`;
-  //   reviewRating = `Rating: ${data.reviews[0].rating}`;
-  // } else {
-  // }
-
+  console.log("isEditMode?: ", isEditMode);
   return (
     <ProductCard>
       <h2>{data.name}</h2>
@@ -36,21 +60,26 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <StyledButton type="button" onClick={() => setIsEditMode(!isEditMode)}>
+        Edit Fish
+      </StyledButton>
+      {isEditMode && (
+        <ProductForm onSubmit={handleEditProduct}>Edit Fish</ProductForm>
+      )}
+      {/* <StyledButton type="button" onClick={() => handleDeleteProduct(id)}>
+        Delete Fish
+      </StyledButton> */}
       {data.reviews.map((review) => {
-        return <p key={review._id}>{review.title}</p>;
+        return (
+          <div key={review._id}>
+            <h3>Review:</h3>
+            <p>Title: {review.title}</p>
+            <p>Text: {review.text}</p>
+            <p>Rating: {review.rating}</p>
+          </div>
+        );
       })}
-      {/* <div>
-        <h3>{reviewHeadline}</h3>
-        <p>{reviewTitle}</p>
-        <p>{reviewText}</p>
-        <p>{reviewRating}</p>
-      </div> */}
-      <div>
-        {/* {if (!data.review) {<p>No Review available</p>} else (<h2>Review:</h2>
-      <p>Title: {data.review.title}</p>
-      <p>Text: {data.review.text}</p>
-      <p>Rating: {attachClipboardStubToView.review.rating}</p>)} */}
-      </div>
+
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -4,6 +4,8 @@ import Product from "@/db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
+
+  // GET request:
   if (request.method === "GET") {
     const product = await Product.findById(id).populate("reviews");
     if (!product) {
@@ -11,5 +13,16 @@ export default async function handler(request, response) {
     }
 
     response.status(200).json(product);
+  }
+  // PUT request
+  const updatedProduct = request.body;
+  if (request.method === "PUT") {
+    const product = await Product.findByIdAndUpdate(id, updatedProduct);
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json({ status: `Product ${id} updated!` });
+    // If successful, you'll receive an OK status code.
   }
 }
